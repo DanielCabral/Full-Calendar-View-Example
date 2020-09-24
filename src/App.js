5e35ca651fef22d33ced1825bdc8048722ba5a7f
Francisco Claudeilton Dantas <claudeilton.dantas@MATERCHRISTI.local>
2020-09-23 21:51:28 -0300

    Working full calendar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import FullCalendar, { formatDate } from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import { INITIAL_EVENTS, createEventId } from './event-utils'
 
-function App() {
+
+
+export default class App extends React.Component {
+  calendarComponentRef = React.createRef();
+  render() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">    
+      <FullCalendar
+            locale='pt-br'
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            headerToolbar={{
+              left: 'prev,next',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            }}
+            initialView='timeGridWeek'
+            buttonText={{
+              today:    'Hoje',
+              month:    'Mensal',
+              week:     'Semanal',
+              day:      'Diario'
+            }}   
+            views = {[
+              {
+                timeGridDay: { // name of view
+                titleFormat: { year: 'string', month: '4-digit', day: '4-digit' }
+                // other view-specific options here
+              }
+            }
+          ]}
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}            
+            initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+            eventClick={()=> {alert() }}
+            select={()=>{let calendarApi = this.calendarComponentRef.current.getApi();
+           if(calendarApi.view.type==="dayGridMonth" || calendarApi.view.type==="timeGridWeek");
+            return this.gotoPast();
+          }}
+            ref={this.calendarComponentRef}
+            /* you can update a remote database when these fire:
+            eventAdd={function(){}}
+            eventChange={function(){}}
+            eventRemove={function(){}}
+            */
+          />
     </div>
   );
 }
-
-export default App;
+gotoPast = () => {
+  let calendarApi = this.calendarComponentRef.current.getApi();
+  calendarApi.changeView("timeGridDay");
+};
+}
---
+++ src/App2.js
@@ -0,0 +1,77 @@
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
+
+//import "./styles.css";
+
+// must manually import the stylesheets for each plugin
+//import "@fullcalendar/core/main.css";
+//import "@fullcalendar/daygrid/main.css";
+//import "@fullcalendar/timegrid/main.css";
+
+export default class DemoApp extends React.Component {
+  calendarComponentRef = React.createRef();
+
+  state = {
+    calendarWeekends: true,
+    calendarEvents: [
+      // initial event data
+      { title: "Event Now", start: new Date() }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="demo-app">
+        <div className="demo-app-top">
+          <button onClick={this.toggleWeekends}>toggle weekends</button>&nbsp;
+          <button onClick={this.gotoPast}>go to day view</button>
+          &nbsp; (also, click a date/time to add an event)
+        </div>     
+          <FullCalendar
+            defaultView="dayGridMonth"
+            header={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
+            }}
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            ref={this.calendarComponentRef}
+            weekends={this.state.calendarWeekends}
+            events={this.state.calendarEvents}
+            dateClick={this.gotoPast}
+            
+            
+          />
+      </div>
+    );
+  }
+
+  toggleWeekends = () => {
+    this.setState({
+      // update a property
+      calendarWeekends: !this.state.calendarWeekends
+    });
+  };
+
+  gotoPast = () => {
+    let calendarApi = this.calendarComponentRef.current.getApi();
+    calendarApi.changeView("timeGridDay");
+  };
+
+  handleDateClick = arg => {
+    if (true) {
+      this.setState({
+        // add new event data
+        calendarEvents: this.state.calendarEvents.concat({
+          // creates a new array
+          title: "New Event",
+          start: arg.date,
+          allDay: arg.allDay
+        })
+      });
+    }
+  };
+}
